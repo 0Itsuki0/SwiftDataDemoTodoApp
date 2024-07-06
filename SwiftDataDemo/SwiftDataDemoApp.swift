8f84ce53a3488bdf24944fd598d13e369ded8054
itsuki <itsuki.enjyoy@gmail.com>
2024-07-06 15:07:06 +0900

    Initial Commit

---
+++ SwiftDataDemo/SwiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemoApp.swift
+//  SwiftDataDemo
+//
+//  Created by Itsuki on 2024/07/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
